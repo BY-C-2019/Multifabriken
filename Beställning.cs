5bc6e61ff6f7b06a41ec20bfec8df7787185ac26
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-10-02 11:53:07 +0200

    Updated with class beställning

---
+++ Beställning.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Multifabriken
+{
+    class Beställning
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
