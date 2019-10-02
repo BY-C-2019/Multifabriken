04d8cc7772f46c4d8f516a824649e3e00b697f47
Lukas Selin <lukas.selin@viskan.se>
2019-10-02 14:51:44 +0200

    Milk done

---
+++ milk.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace milk
+{
+    class Milk
+    {
+        public Milk(int fatContent, int packageType)
+        {
+            if(11 < fatContent || fatContent < 2)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            fat = fatContent;            
+            package = packageList[packageType];
+        }
+        // Fetthalt kan vara mellan 2 och 11
+        public int fat{get; set;} = 2; 
+        // Olika typer av förpackningar
+        private List<string> packageList{get;} = new List<string>(){"1 liter", "2 liter", "0.5 liter"};
+
+        public string package = "";
+    }
+
+}
+
