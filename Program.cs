ac2b5420d6c330eefcbd648b049aa20dbc9c95be
Nils Abrahamson <grunau@gmx.de>
2019-10-02 15:15:31 +0200

    skriv ut listorna

--- Program.cs
+++ Program.cs
@@ -153,11 +153,88 @@ namespace Multifabriken
 
                     case "3":
                         break;
-
+                    // mjölkmeny
                     case "4":
-                        break;
+                        // instans skapas
+                        Oatmilk myMilk = new Oatmilk();
 
+                        Console.WriteLine("Välj mängd:");
+                        Console.WriteLine("[1] 0,5 liter");
+                        Console.WriteLine("[2] 1 liter");
+                        Console.WriteLine("[3] 2 liter");
+                        input = Console.ReadLine();
+
+                        if (input == "1")
+                        {
+                            myMilk.Volume = "0,5 liter";
+                        }
+                        else if (input == "2")
+                        {
+                            myMilk.Volume = "1 liter";
+                        }
+                        else
+                        {
+                            myMilk.Volume = "2 liter";
+                        }
+
+                        Console.WriteLine("Välj fetthalt:");
+                        Console.WriteLine("[1] 0,5%");
+                        Console.WriteLine("[2] 1,5%");
+                        Console.WriteLine("[3] 3%");
+                        input = Console.ReadLine();
+
+                        if (input == "1")
+                        {
+                            myMilk.Fat = "0,5%";
+                        }
+                        else if (input == "2")
+                        {
+                            myMilk.Fat = "1,5%";
+                        }
+                        else
+                        {
+                            myMilk.Fat = "3%";
+                        }
+
+
+                        ListOfMilk.Add(myMilk);
+
+                        break;
+                    // skriv ut listorna
                     case "5":
+                        Console.WriteLine("Inköpslistan:");
+                        Console.WriteLine("");
+
+                        foreach (var item in ListOfCars)
+                        {
+                            Console.WriteLine(item.Model);
+                            Console.WriteLine(item.Color);
+                            Console.WriteLine(item.Luxury);
+                            Console.WriteLine("");
+                        }
+
+                        foreach (var item in ListOfPipes)
+                        {
+                            Console.WriteLine(item.Material);
+                            Console.WriteLine(item.Diameter);
+                            Console.WriteLine(item.Längd);
+                            Console.WriteLine("");
+                        }
+
+                        foreach (var item in ListOfGodis)
+                        {
+                            Console.WriteLine(item.Smak);
+                            Console.WriteLine(item.Vikt);
+                            Console.WriteLine("");
+                        }
+
+                        foreach (var item in ListOfMilk)
+                        {
+                            Console.WriteLine(item.Volume);
+                            Console.WriteLine(item.Fat);
+                            Console.WriteLine("");
+                        }
+
                         break;
 
                     case "6":
