ef08895c37d97fa1eadcc929094028017ea0990f
Jonatan Schultz <jonatan_schultz@hotmail.com>
2019-10-02 15:20:02 +0200

    Konstruktor

--- Bilar.cs
+++ Bilar.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Bilar
+namespace bilar
 {
     class Bilar
-    {
+    {   
+        public Bilar(int Carmodelindex, string Carcolour, bool Luxpackage)
+        {
+            carmodel = Billista[Carmodelindex];
+            carcolour = Carcolour;
+            luxpackage = Luxpackage;
+
+            
+        }
         static List<string> Billista = new List<string>() { "Tesla", "Audi", "Hyundai" };
         public string carmodel { get; set; }
         public string carcolour { get; set; }
         public bool luxpackage { get; set; }
-
-        public Bilar(string Carmodel, string Carcolour, bool Luxpackage)
-        {
-            carmodel = Carmodel;
-            carcolour = Carcolour;
-            luxpackage = Luxpackage;
-        }
+ 
     }
 }
 
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using bilar;
+
+namespace Program
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Bilar myBilar = new Bilar("Tesla", "Blå", true);
+
+            Console.WriteLine(myBilar.carmodel);
+            Console.WriteLine(myBilar.carcolour);
+            Console.WriteLine(myBilar.luxpackage);
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
