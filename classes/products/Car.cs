202c95e29fc798b1627216ee991d9c400012c23e
Erik Skinnari <erikskinnari@gmail.com>
2019-10-02 18:03:04 +0200

    Tidy up. Cars

--- classes/products/Car.cs
+++ classes/products/Car.cs
@@ -11,21 +11,20 @@ namespace Multifabriken
         {
                 if(LuxuryInteriour)
                 {
-                    return "Modell: "+ Brand+ " Färg: "+Color+ " Lyxutrustad: Ja! Pris: "+Price+"kr";
+                    return "Modell: "+ Brand+ " Färg: "+Color+ " Lyxutrustad: Ja! Pris: "+Price+" kr";
                 }
                 else
                 {
-                       return "Modell: "+ Brand+ " Färg: "+Color+ " Lyxutrustad: Nej! Pris: "+Price+"kr";
+                       return "Modell: "+ Brand+ " Färg: "+Color+ " Lyxutrustad: Nej! Pris: "+Price+" kr";
                 }
-                
         }
         public Car()
         {
-            Price = 120000;            
+            Price = 120000;
             string[] carBrands = new string[]{"Vas lada","Honda","Volvo","MeserSchmidt","Saab","Audi","Wolkswagen","Lotus"};
             string[] colors = new string[]{"Svart","Vit","Grön","Gul","Rosa","Orange","Blå","Silvermetallic"};
 
-            Brand= carBrands[Menu.DisplayMenu(carBrands,"")];            
+            Brand= carBrands[Menu.DisplayMenu(carBrands,"")];
             Color = colors[Menu.DisplayMenu(colors,"")];
             Name = Color + " " + Brand;
             LuxuryInteriour = Menu.YesOrNo("Vill du ha lyxpaketet?");
