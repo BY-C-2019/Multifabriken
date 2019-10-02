a0f0bd9bbe8b74e92e4f4eb6dfb3e622a4421bbe
Erik Skinnari <erikskinnari@gmail.com>
2019-10-02 11:27:59 +0200

    Added Order class

---
+++ Order.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace Multifabriken
+{
+    public class Order
+    {
+        public List<Product> Products { get; set; }
+
+        public bool Confimed { get; set; } = false;
+
+        public decimal TotalPrice() 
+        {
+            decimal total = 0;
+
+            foreach (var product in Products)
+            {
+                total += product.Price;
+            }
+
+            return total;
+        }
+
+        public int NumOfProducts()
+        {
+            return Products.Count;
+        }
+    }
+}
