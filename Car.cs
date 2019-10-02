b0ef2b7ac3f2a23e4d787696ebf10c01da99f8cf
Nils Abrahamson <grunau@gmx.de>
2019-10-02 13:21:41 +0200

    added Car.cs

---
+++ Car.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Multifabriken
+{
+    class Car
+    {
+        private string model = "";
+        private string color = "";
+        private bool luxury = true;
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public bool Luxury { get; set; }
+
+    }
+}
