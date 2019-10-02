d66477ec1cb37faef335b9dee64d8f5ecc5a3429
Benjamin Ben Taleb <benjamiiiin94@gmail.com>
2019-10-02 14:35:54 +0200

    Skapar Pipes-class

---
+++ Pipes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pipes
+{
+  class Pipes
+  {
+    static List<string> myPipes = new List<string>() {
+          "Rostfritt",
+          "Betong",
+          "Stål"
+      };
+    public int length { get; set; }
+    public int diameter { get; set; }
+    public string material = "";
+    public Pipes(int Length, int Diameter, string Material)
+    {
+      length = Length;
+      diameter = Diameter;
+      material = Material;
+    }
+  }
+}
+
+
+
+
+
---
+++ Varor.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Multifabriken
+{
+  class Varor
+  {
+    public int antal { get; set; }
+    public int mängd { get; set; }
+  }
+}
