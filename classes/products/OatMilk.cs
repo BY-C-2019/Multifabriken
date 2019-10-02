a1225a1f2374c9396a0d2df22feb75d8220098b4
Erik Skinnari <erikskinnari@gmail.com>
2019-10-02 15:21:39 +0200

    Fiddeling

--- classes/products/OatMilk.cs
+++ classes/products/OatMilk.cs
@@ -9,16 +9,17 @@ namespace Multifabriken
 
         public OatMilk()
         {
-            string[] fatRatioStrings = new string[]{"0.5% fetthalt","1.0% fetthalt","1.5% fetthalt","4.0% fetthalt"};
-            double[] fatRatio = new double[]{0.5, 1.0, 1.5, 4.0};
-            string[] sizeStrings = new string[]{"0.5 liter","1.0 liter","2.0 liter"};
-            double[] sizes = new double[]{ 0.5, 1, 2};
-            int fatIndex = Menu.DisplayMenu(fatRatioStrings,"");
-            int sizeIndex = Menu.DisplayMenu(sizeStrings,"");
-            Size = sizes[sizeIndex];
-            FatRatio = fatRatio[fatIndex];
-            Name = Size + " liter havremjölk med " + FatRatio + "% fetthalt";
+            string[] fatRatioStrings = new string[]{"0.5% fetthalt","1.0% fetthalt","1.5% fetthalt","4.0% fetthalt","11.0% fetthalt"};
+            double[] fatRatio = new double[]{0.5, 1.0, 1.5, 4.0, 11.0};
+            FatRatio = fatRatio[Menu.DisplayMenu(fatRatioStrings,"")];
+            Name = "Havremjölk";
+            Size = Menu.MenuForAmount("Hur många liter önskas? ", 30, false);
             Price = Convert.ToDecimal(15 * Size);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Size} liter, Fetthalt {FatRatio}%. Summa: {Price} kr.";
         }
     }
 }
