340b45633b3828171bdfd0c16b989871520b6c94
Jonatan Schultz <jonatan_schultz@hotmail.com>
2019-10-02 14:44:49 +0200

    added class bilar

---
+++ Bilar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bilar
+{
+    class Bilar
+    {
+        static List<string> Billista = new List<string>() { "Tesla", "Audi", "Hyundai" };
+        public string carmodel { get; set; }
+        public string carcolour { get; set; }
+        public bool luxpackage { get; set; }
+
+        public Bilar(string Carmodel, string Carcolour, bool Luxpackage)
+        {
+            carmodel = Carmodel;
+            carcolour = Carcolour;
+            luxpackage = Luxpackage;
+        }
+    }
+}
+
