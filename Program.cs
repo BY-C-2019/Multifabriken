0941d62a67122b2a7457a6b214b563ac36f5751e
Philip-vester <phvesterlund@gmail.com>
2019-10-02 16:55:34 +0200

    Start of implementing menu

--- Program.cs
+++ Program.cs
@@ -8,6 +8,49 @@ namespace Multifabriken
         static void Main(string[] args)
         {
             List<string> myCart = new List<string>();
+
+            Console.WriteLine("Hej och välkommen till Multifabriken AB! \n(nummer + enter)");
+            Console.WriteLine("\n[1]Produkter \n[2]Kundvagn \n[3]Avsluta");
+            int input = Convert.ToInt32(Console.ReadLine());
+            string[] products =  {"Bil", "Godis", "Rör", "Havremjölk"};
+
+            switch(input)
+                        {
+                        
+                        case 1:
+                                Console.WriteLine("Vilken produkt vill du lägga till i din kundvagn?");
+
+                                foreach (string thing in products)
+                                {
+                                    Console.WriteLine(thing);
+                                }
+                                int input2 = Convert.ToInt32(Console.ReadLine());
+
+                                    switch(input2)
+                                    {
+                                        //tillfälligt för test
+                                        case 1:
+                                            Candy myCandy = new Candy();
+                                            myCandy.CandyMenu();
+                                            myCandy.AddToCart(myCart);
+                                            
+                                                foreach (var product in myCart)
+                                                    {
+                                                        Console.WriteLine(product);
+                                                    }
+                                            break;
+                                }
+                                break;
+                        case 2:
+                                Console.WriteLine("Kundvagn: ");
+                                foreach (var product in myCart)
+                                                    {
+                                                        Console.WriteLine("\n \t" + product);
+                                                    }
+                                break;
+                        case 3:
+                                
+                        }    
         }
     }
 }
