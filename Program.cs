4e471523d1fdf064a7b443e2e12d01b63f417e18
Johannes Andersson <johannes_hp@hotmail.com>
2019-10-03 16:41:36 +0200

    Continued and did some updates on mainmenu

--- Program.cs
+++ Program.cs
@@ -7,50 +7,82 @@ namespace Multifabriken
     {
         static void Main(string[] args)
         {
-            List<string> myCart = new List<string>();
+            // Meny variabler
+            string[]        menuOptions = { "Produkter", "Kundvagn", "Avsluta" };
 
-            Console.WriteLine("Hej och välkommen till Multifabriken AB! \n(nummer + enter)");
-            Console.WriteLine("\n[1]Produkter \n[2]Kundvagn \n[3]Avsluta");
-            int input = Convert.ToInt32(Console.ReadLine());
-            string[] products =  {"Bil", "Godis", "Rör", "Havremjölk"};
+            List<string>    myCart = new List<string>();
+            string[]        currentProducts =  {"Bil", "Godis", "Rör", "Havremjölk"};
 
-            switch(input)
+            // Program loop
+            while (true) 
+            {
+                // Skriv ut meny och alternativ
+                Console.WriteLine("Hej och välkommen till Multifabriken AB!");
+                Console.WriteLine("----------------------------------------");
+                for (int i = 0; i < menuOptions.Length; i++)
+                {
+                    Console.WriteLine("[{0}] {1}", i+1, menuOptions[i]);
+                }
+                string input = Console.ReadLine();
+
+                switch(input[0])
+                {
+                    case '1':
+                    // Lista tillgängliga produktval för användaren att välja
+                    Console.WriteLine("Vilken produkt vill du lägga till i din kundvagn?");
+                    foreach (string product in currentProducts)
+                    {
+                        Console.WriteLine(product);
+                    }
+                    // Läs in användarens val och välj produkt
+                    int input2 = Convert.ToInt32(Console.ReadLine());
+
+                        switch(input2)
                         {
-                        
-                        case 1:
-                                Console.WriteLine("Vilken produkt vill du lägga till i din kundvagn?");
-
-                                foreach (string thing in products)
-                                {
-                                    Console.WriteLine(thing);
-                                }
-                                int input2 = Convert.ToInt32(Console.ReadLine());
-
-                                    switch(input2)
-                                    {
-                                        //tillfälligt för test
-                                        case 1:
-                                            Candy myCandy = new Candy();
-                                            myCandy.CandyMenu();
-                                            myCandy.AddToCart(myCart);
-                                            
-                                                foreach (var product in myCart)
-                                                    {
-                                                        Console.WriteLine(product);
-                                                    }
-                                            break;
-                                }
-                                break;
-                        case 2:
-                                Console.WriteLine("Kundvagn: ");
-                                foreach (var product in myCart)
-                                                    {
-                                                        Console.WriteLine("\n \t" + product);
-                                                    }
-                                break;
-                        case 3:
+                            //tillfälligt för test
+                            case 1:
+                                Candy myCandy = new Candy();
+                                myCandy.CandyMenu();
+                                myCandy.AddToCart(myCart);
                                 
-                        }    
+                                    foreach (var product in myCart)
+                                        {
+                                            Console.WriteLine(product);
+                                        }
+                                break;
+                    }
+                    break;
+                
+                    // Lista kundvagn
+                    case '2':
+                    Console.WriteLine("Din nuvarande kundvagn: ");
+                    PrintProductsInCart(myCart);
+                    break;
+                    
+                    // Avsluta programmet
+                    case '3':
+                    Console.WriteLine("Du hann inte checka ut din kundvagn:");
+                    // Skriv ut kundvagnen
+                    PrintProductsInCart(myCart);
+                    
+                    // Avsluta programmet med return
+                    Console.WriteLine("Programmet avslutas...");
+                    return;
+
+                    // Felinmatningar
+                    default:
+                    Console.WriteLine("Felinmatning...");
+                    break;
+                }
+            }
+        }
+
+        static void PrintProductsInCart(List<string> cartToPrint)
+        {
+            foreach (var product in cartToPrint)
+            {
+                Console.WriteLine("\n \t" + product);
+            }
         }
     }
 }
