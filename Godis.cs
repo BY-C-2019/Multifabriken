36bbe97dfe8bb976a0b7c6638d5cb8697db5e2bc
Noxheart <daniel-97@live.se>
2019-10-05 22:33:55 +0200

    Godis

--- Godis.cs
+++ Godis.cs
@@ -1,9 +1,10 @@
+using System;
 namespace Multifabriken
 {
     class Godis
     {
-        private string vikt = "";
-        private string smak = "";
+        private static string vikt = "";
+        private static string smak = "";
 
         public string Vikt
         {
@@ -27,10 +28,76 @@ namespace Multifabriken
                 smak = value;
             }
         }
-        static void AddGodis()
+        public static void AddGodis()
         {
+            string input;
 
+            bool loop = false;
 
+            while (!loop)
+            {
+                Console.WriteLine("Välj smak");
+                Console.WriteLine("[1] Hallon");
+                Console.WriteLine("[2] Jordgubb");
+                Console.WriteLine("[3] Lakris");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        smak = "Hallon";
+                        loop = true;
+                    break;
+
+                    case "2":
+                        smak = "Jordgubb";
+                        loop = true;
+                    break;
+
+                    case "3":
+                        smak = "Lakris";
+                        loop = true;
+                    break;
+
+                    default:
+                        Console.WriteLine("Vänligen ange endast ett nummer mellan 1 och 3");
+                    break;
+
+                }
+            }
+
+            loop = false;
+            
+            while (!loop)
+            {
+                Console.WriteLine("Välj vikt");
+                Console.WriteLine("[1] 100g");
+                Console.WriteLine("[2] 200g");
+                Console.WriteLine("[3] 300g");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        vikt = "100g";
+                        loop = true;
+                    break;
+
+                    case "2":
+                        vikt = "200g";
+                        loop = true;
+                    break;
+
+                    case "3":
+                        vikt = "300g";
+                        loop = true;
+                    break;
+
+                    default:
+                        Console.WriteLine("Vänligen ange endast ett nummer mellan 1 och 3");
+                    break;
+                }
+            }
         }
 
 
