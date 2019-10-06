91a9f9d2d873ca508415d8cf142b01120385857a
Adam Björk <adamhbjork@gmail.com>
2019-10-07 00:04:47 +0200

    Felhantering i Pipe.cs

--- Pipe.cs
+++ Pipe.cs
@@ -8,92 +8,180 @@ namespace Multifabriken
         private string materialType;
         private int diameter;
         private int length;
-        bool loop = true;
+        private int input;
+        bool loop = false;
         string[] availableTypes = { "Plast", "Betong", "Stål" };
 
         //Metod som ska anropas från Main
-        //Enbart till för tydlighet så inte all kod ligger i samma metod
+        //Till för tydlighet så inte all kod ligger i samma metod
         public bool OrderPipe()
         {
             ChoosePipeType();
             ChooseDiameter();
             ChooseLength();
 
-            while (loop)
+            do
             {
-                Console.WriteLine("Typ: " + materialType);
-                Console.WriteLine("Diameter: " + diameter + " millimeter");
-                Console.WriteLine("Längd: " + length + " meter");
-                Console.Write("Vill du lägga till i kundvagn? Ja/Nej: ");
-                string WannaAddToCart = Console.ReadLine();
-                WannaAddToCart = WannaAddToCart.ToUpper();
+                loop = false;
 
-                //Returnerar true/false till Main ifall ordern ska läggas till i kundvagnen
-                if (WannaAddToCart == "JA")
+                try
                 {
-                    loop = false;
-                    Console.WriteLine("Din rörorder har lagts till i din kundvagn!");
-                    return true;
+                    Console.WriteLine("Typ: " + materialType);
+                    Console.WriteLine("Diameter: " + diameter + " millimeter");
+                    Console.WriteLine("Längd: " + length + " meter");
+                    Console.Write("Vill du lägga till i kundvagn? Ja/Nej: ");
+                    string addToCartInput = Console.ReadLine();
+                    addToCartInput = addToCartInput.ToUpper();
+
+                    //Returnerar true/false till Main ifall ordern ska läggas till i kundvagnen
+                    if (addToCartInput[0] == 'J')
+                    {
+                        Console.WriteLine("Din rörorder har lagts till i din kundvagn!");
+                        Console.ReadLine();
+                        return true;
+                    }
+                    else if (addToCartInput[0] == 'N')
+                    {
+                        Console.WriteLine("Du valde att inte lägga till något i kundvagen.");
+                        Console.ReadLine();
+                        return false;
+                    }
+                    else
+                    {
+                        loop = true;
+                        Console.WriteLine("Programmet accepterar bara Ja eller Nej som svar");
+                        Console.ReadLine();
+                    }
                 }
-                else if (WannaAddToCart == "NEJ")
+                catch
                 {
-                    loop = false;
-                    Console.WriteLine("Du valde att inte lägga till något i kundvagen.");
-                    return false;
-                }
-                else
-                {
-                    Console.WriteLine("Endast Ja eller Nej");
+                    loop = true;
+                    Console.WriteLine("Programmet accepterar bara Ja eller Nej som svar");
+                    Console.ReadLine();
                 }
             }
+            while (loop);
+
             return false;
 
         }
         //Låter kunden välja typ av rör
         public void ChoosePipeType()
         {
-            Console.WriteLine("Rörtyper som finns att beställa: ");
-            for (int i = 0; i < availableTypes.Length; i++)
+            do
             {
-                Console.WriteLine("[" + (i) + "] " + availableTypes[i]);
-            }
+                loop = false;
 
-            Console.Write("Välj typ: ");
-            int pipeTypeWanted = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    Console.WriteLine("Rörtyper som finns att beställa: ");
+                    for (int i = 0; i < availableTypes.Length; i++)
+                    {
+                        Console.WriteLine("[" + i + "] " + availableTypes[i]);
+                    }
 
-            switch (pipeTypeWanted)
-            {
-                case 0:
-                    materialType = availableTypes[pipeTypeWanted];
-                    break;
-                case 1:
-                    materialType = availableTypes[pipeTypeWanted];
-                    break;
-                case 2:
-                    materialType = availableTypes[pipeTypeWanted];
-                    break;
+                    Console.Write("Välj typ: ");
+                    int pipeTypeWanted = Convert.ToInt32(Console.ReadLine());
+
+                    switch (pipeTypeWanted)
+                    {
+                        case 0:
+                            materialType = availableTypes[pipeTypeWanted];
+                            break;
+                        case 1:
+                            materialType = availableTypes[pipeTypeWanted];
+                            break;
+                        case 2:
+                            materialType = availableTypes[pipeTypeWanted];
+                            break;
+                        default:
+                            loop = true;
+                            Console.WriteLine("Gör ett val mellan 0-2");
+                            Console.ReadLine();
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Endast heltal!");
+                    Console.ReadLine();
+                    loop = true;
+                }
             }
+            while (loop);
+
             Console.WriteLine("Du har valt rör av typen " + materialType + ".");
             Console.ReadLine();
         }
         //När kunden valt rörtyp får hen välja diametern på röret
         public void ChooseDiameter()
         {
-            Console.Write("Vilken diameter i millimeter ska " + materialType + "röret ha? ");
-            diameter = Convert.ToInt32(Console.ReadLine());
-            Console.ReadLine();
+            do
+            {
+                loop = false;
+
+                try
+                {
+                    Console.Write("Vilken diameter i millimeter ska " + materialType + "röret ha? ");
+                    input = Convert.ToInt32(Console.ReadLine());
+
+                    if (input > 1)
+                    {
+                        diameter = input;
+                    }
+
+                    else if (input < 2)
+                    {
+                        loop = true;
+                        Console.WriteLine("Diametern måste vara minst 2mm");
+                        Console.ReadLine();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Felaktig inmatning. Endast siffror och det måste vara hela millimeter.");
+                    Console.ReadLine();
+                    loop = true;
+                }
+            }
+            while (loop);
         }
         //Efter att rörtyp och diameter valts väljer kunden hur många meter rör hen vill beställa
         public void ChooseLength()
         {
-            Console.WriteLine("Hur många meter rör vill du beställa?");
-            length = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                loop = false;
+
+                try
+                {
+                    Console.WriteLine("Hur många meter rör vill du beställa?");
+                    input = Convert.ToInt32(Console.ReadLine());
+                    if(input >= 1)
+                    {
+                        length = input;
+                    }
+                    else if (input < 1)
+                    {
+                        loop = true;
+                        Console.WriteLine("Du måste beställa minst en meter.");
+                    }
+                }
+                catch
+                {
+                    loop = true;
+                    Console.WriteLine("Endast siffror och hela meter!");
+                    Console.ReadLine();
+                }
+            }
+            while (loop);
+
             Console.ReadLine();
         }
         //Metod för att addera string med beställning till lista
         public void AddToCart(List<string> pipeToAddToList)
         {
-            pipeToAddToList.Add("Typ: " + materialType + ". Diameter : " + diameter + "mm. Längd: " + length + " meter.");
+            pipeToAddToList.Add("Typ av rör: " + materialType + ". Diameter : " + diameter + "mm. Längd: " + length + " meter.");
         }
     }
 }
