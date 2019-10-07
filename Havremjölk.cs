6082c612adbd287dfc157a4c345764cecbcedf8f
Alexander <alex.esbj@gmail.com>
2019-10-07 18:27:11 +0200

    Bugfix - mjölk
    Fixar bugg som orsakade att mjölken läggs till även om det är utanför tillåtna fetthalt

--- Havremjölk.cs
+++ Havremjölk.cs
@@ -2,32 +2,35 @@ using System;
 
 namespace Multifabriken
 {
-public class Havremjölk
+    public class Havremjölk
     {
         //Skapar konstruktor som sätter standardvärde på objektet när de skapas.
-        public Havremjölk (int fetthalt, double volym)
+        public Havremjölk(int fetthalt, double volym)
         {
             Fetthalt = fetthalt;
             Volym = volym;
         }
-        
-        //Metod för fetthalt på mjölk
-        public static int milkFat(){
-        Console.WriteLine("Vilken fetthalt vill du ha? ");
-        Console.WriteLine("(Mellan 2% och 11%)");
-        string fat = Console.ReadLine();
-        int partFat = 0;    
 
-        try{
-        partFat = Convert.ToInt32(fat);
-        }catch{
-        Console.WriteLine("Ange fetthalt i heltal");
-        }
-        //byt till do while
-        if (partFat < 2 || partFat > 11){
-        Console.WriteLine("Fetthalten måste vara mellan 2% och 11%");
-        }
-        return partFat;
+        //Metod för fetthalt på mjölk
+        public static int milkFat()
+        {
+            int partFat = 0;
+            do
+            {
+                Console.WriteLine("Vilken fetthalt vill du ha? ");
+                Console.WriteLine("(Mellan 2% och 11%)");
+                string fat = Console.ReadLine();
+
+                try
+                {
+                    partFat = Convert.ToInt32(fat);
+                }
+                catch
+                {
+                    Console.WriteLine("Ange fetthalt i heltal");
+                }
+            } while (partFat < 2 || partFat > 11);
+            return partFat;
         }
 
         //properties
