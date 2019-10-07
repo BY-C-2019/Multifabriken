a6e20688ca45f116ba824dc0027a1a7b0183c7cf
Lukas Selin <lukas.selin@viskan.se>
2019-10-07 16:38:12 +0200

    Multifabriken är färdig! Behövs felhantering (Jo)

--- milk.cs
+++ milk.cs
@@ -20,7 +20,6 @@ namespace Milk
         // Fetthalt kan vara mellan 2 och 11
         public int fat{get; set;} = 2; 
         // Olika typer av förpackningar
-        private List<string> packageList{get;} = new List<string>(){"1 liter", "2 liter", "0.5 liter"};
 
         public string package = "";
     }
