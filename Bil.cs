e5539060dc38d0266240a64594f734d5fa8b04c6
Joakim Kvist <joakim.kvist@outlook.com>
2019-10-07 11:15:44 +0200

    Kopplar till kvitto + tillägg på beställning.

--- Bil.cs
+++ Bil.cs
@@ -7,53 +7,54 @@ namespace Multifabriken
         private int choiceModell = 0;
         private int choiceFärg = 0;
         private int choiceInredning = 0;
+        string ValAvModell;
+        string ValAvFärg;
+        string ValAvInredning;
+        string BilTillKvitto;
         
-        public Bil()
-        {
-            Console.WriteLine("Här kan du göra beställning av en bil.");
-            Console.WriteLine("Du kommer få välja modell, färg och inrednign.");
-        }
         public void ValAvBil()
        {
+            Kvitto kvitto = new Kvitto();
+            Produkter bil = new Produkter();
+            
+            Console.Clear();
+            Console.WriteLine("Här kan du göra beställning av en bil.");
+            Console.WriteLine("Du kommer få välja modell, färg och inrednign.");
             Console.WriteLine("Välj av följande modeller:");
             Console.WriteLine("[1] Volvo");
             Console.WriteLine("[2] SAAB");
             Console.WriteLine("[3] BMW");
-            Console.WriteLine("[4] Gå tillbaka till huvudmenyn");
             choiceModell = int.Parse(Console.ReadLine());
             // choiceToCustomerModell = int.Parse(Console.ReadLine());
             switch (choiceModell)
             {
                 case 1: 
                 Console.Clear();
-                Console.WriteLine("Du valde en Volvo"); 
+                Console.WriteLine("Du valde en Volvo");
+                ValAvModell = "Modell: Volvo."; 
                 break;
                 
                 case 2: 
                 Console.Clear();
                 Console.WriteLine("Du valde en SAAB");
+                ValAvModell = "Modell: SAAB.";
                 break;
 
                 case 3: 
                 Console.Clear();
                 Console.WriteLine("Du valde en BMW");
+                ValAvModell = "Modell: BMW.";
                 break;
                 
-                case 4:
-                Console.Clear();
-                break;
                 default:
                 Console.WriteLine("Felaktig inmatning");
                 break;
             }
-       
-                        
+
             Console.WriteLine("Välj av följande färger:");
             Console.WriteLine("[1] Röd");
             Console.WriteLine("[2] Blå");
             Console.WriteLine("[3] Grön");
-            Console.WriteLine("[4] Byt modell");
-            Console.WriteLine("[5] Gå tillbaka till huvudmenyn");
             choiceFärg = int.Parse(Console.ReadLine());
             // choiceToCustomerModell = int.Parse(Console.ReadLine());
             switch (choiceFärg)
@@ -61,63 +62,64 @@ namespace Multifabriken
                 case 1: 
                 Console.Clear();
                 Console.WriteLine("Du valde röd färg"); 
+                ValAvFärg = "Färg: Röd.";
                 break;
                 
                 case 2: 
                 Console.Clear();
-                Console.WriteLine("Du valde en blå färg");
+                Console.WriteLine("Du valde blå färg");
+                ValAvFärg = "Färg: Blå.";
                 break;
 
                 case 3: 
                 Console.Clear();
                 Console.WriteLine("Du valde en grön färg");
-                break;
-                
-                case 4:
-                Console.Clear();
-                Console.WriteLine("Du valde att byta modell");
-                break;
-                
-                case 5:
-                Console.Clear();
-                Console.WriteLine("Återgår till huvudmenyn.");
+                ValAvFärg = "Färg: Grön.";
                 break;
 
                 default:
                 Console.WriteLine("Felaktig inmatning");
                 break;
             }
-            
-          
-            
             Console.WriteLine("Välj av följande inredningspaket:");
             Console.WriteLine("[1] Lyx");
             Console.WriteLine("[2] Mellan");
             Console.WriteLine("[3] Budget");
-            Console.WriteLine("[4] Byt Färg");
-            Console.WriteLine("[5] Gå tillbaka till huvudmenyn");
+            Console.WriteLine("[4] Återgå till beställningsmenyn.");
             choiceInredning = int.Parse(Console.ReadLine());
             // choiceToCustomerModell = int.Parse(Console.ReadLine());
             switch (choiceInredning)
             {
                 case 1: 
                 Console.Clear();
-                Console.WriteLine("Du valde Lyxpaketet"); 
+                Console.WriteLine("Du valde Lyxpaketet");
+                ValAvInredning = "Inredning: Lyxpaket.";
+                BilTillKvitto = ValAvModell + " " + ValAvFärg + " " + ValAvInredning;
+                kvitto.Reciept(BilTillKvitto);
+                bil.Order();
                 break;
                 
                 case 2: 
                 Console.Clear();
                 Console.WriteLine("Du valde Mellanpaketet");
+                ValAvInredning = "Inredning: Mellanpaketet.";
+                BilTillKvitto = ValAvModell + " " + ValAvFärg + " " + ValAvInredning;
+                kvitto.Reciept(BilTillKvitto);
+                bil.Order();
                 break;
 
                 case 3: 
                 Console.Clear();
                 Console.WriteLine("Du valde Budgetpaketet");
+                ValAvInredning = "Inredning: Budgetpaketet.";
+                BilTillKvitto = ValAvModell + " " + ValAvFärg + " " + ValAvInredning;
+                kvitto.Reciept(BilTillKvitto);
+                bil.Order();
                 break;
                 
                 case 4:
                 Console.Clear();
-                Console.WriteLine("Du valde att byta färg");
+                Console.WriteLine("Återgå till beställningsmenyn.");
                 break;
                 
                 case 5:
--- Rör.cs
+++ Rör.cs
@@ -8,16 +8,18 @@ namespace Multifabriken
         private int choiceLängd = 0;
         private int choiceMatrial = 0;
         
-        public Rör()
-        {
+        
+        
+        public void ValAvRör()
+       {       
+            Kvitto kvitto = new Kvitto();
+            Produkter rör = new Produkter();  
+            Console.Clear();          
             Console.WriteLine("Här kan du göra beställning av rör.");
             Console.WriteLine("Du kommer få välja diameter, längd och matrial.");
-        }
-        public void ValAvRör()
-       {
             Console.Write("Skriv in diametern i centimeter: ");
             choiceDiameter = Convert.ToInt32(Console.ReadLine());      
-            Console.WriteLine("Du valde " + choiceDiameter + " cm i diametern.");            
+            Console.WriteLine("Du valde " + choiceDiameter + " cm i diameter.");            
             
             Console.Write("Välj längd (centimeter) på röret: ");
             choiceLängd = Convert.ToInt32(Console.ReadLine());
@@ -27,40 +29,37 @@ namespace Multifabriken
             Console.WriteLine("[1] Betong");
             Console.WriteLine("[2] Stål");
             Console.WriteLine("[3] Plast");
-            Console.WriteLine("[4] Välj om diemensioner.");
-            Console.WriteLine("[5] Gå tillbaka till huvudmenyn");
+            string valRör;
             choiceMatrial = int.Parse(Console.ReadLine());
             switch (choiceMatrial)
             {
             case 1: 
                 Console.Clear();
-                Console.WriteLine("Du valde betong"); 
+                Console.WriteLine("Du valde betong");
+                valRör = "Röret: " + choiceDiameter + " cm diameter, " + choiceLängd + " cm i längd." + " Matrial: betong."; 
+                kvitto.Reciept(valRör);
+                rör.Order();
                 break;
                 
                 case 2: 
                 Console.Clear();
                 Console.WriteLine("Du valde stål");
+                valRör = "Röret: " + choiceDiameter + " cm diameter, " + choiceLängd + " cm i längd." + " Matrial: stål.";
+                kvitto.Reciept(valRör);
+                rör.Order();
                 break;
 
                 case 3: 
                 Console.Clear();
                 Console.WriteLine("Du valde plast");
-                break;
-                
-                case 4:
-                Console.Clear();
-                Console.WriteLine("Du valde att byta diemensioner");
-                break;
-                
-                case 5:
-                Console.Clear();
-                Console.WriteLine("Återgår till huvudmenyn.");
+                valRör = "Röret: " + choiceDiameter + " cm diameter, " + choiceLängd + " cm i längd." + " Matrial: plast.";
+                kvitto.Reciept(valRör);
+                rör.Order();
                 break;
 
                 default:
                 Console.WriteLine("Felaktig inmatning");
                 break;
-            
             }
         }
     }
