392baec84dbb12625c9dd3d3c8473755acc89cf4
Joakim Kvist <joakim.kvist@outlook.com>
2019-10-07 11:45:31 +0200

    Uppdaterar Produkter och program.

--- Produkter.cs
+++ Produkter.cs
@@ -8,6 +8,47 @@ namespace Multifabriken
     {
 
       private int order = 0;
+        
+        
+        
+        public void Huvudmeny(){
+             Console.Clear();
+             Console.WriteLine("Välkommen till Multifabriken, ta en titt på vår meny:\n");
+             System.Console.WriteLine("[1] Beställa produkter");
+             System.Console.WriteLine("[2] Se beställda produkter");
+             System.Console.WriteLine("[3] Avsluta");
+             int input = int.Parse(Console.ReadLine());
+
+             switch(input)
+             {
+                 case 1: 
+                // Beställa produkter
+                 Console.Clear();
+                 Produkter beställning = new Produkter();
+                 beställning.Order();
+                 break;
+
+                 case 2:
+                 //Historik
+                 Console.Clear();
+                 Kvitto kvitto = new Kvitto();
+                 kvitto.PrintReciept();
+                 
+                 break;
+
+                 case 3: 
+                 return;
+
+                 default:
+
+                 System.Console.WriteLine("Felaktig inmatning.");
+
+                 break;
+
+             }
+            }
+        
+        
         public void Order()
         {
             Console.Clear();
@@ -15,6 +56,7 @@ namespace Multifabriken
             System.Console.WriteLine("[2] Beställa rör");
             System.Console.WriteLine("[3] Beställa bil");
             System.Console.WriteLine("[4] Beställa Havremjölk");
+            System.Console.WriteLine("[5] Återgå till huvudmeny.");
             order = int.Parse(Console.ReadLine());
 
             switch (order)
@@ -41,6 +83,11 @@ namespace Multifabriken
                 Console.Clear();
                 Havremjölk havremjölk = new Havremjölk();
                 havremjölk.Havremilk();
+                break;
+
+                case 5: 
+                Console.Clear();
+                Huvudmeny();
                 break;
 
                 default:
--- Program.cs
+++ Program.cs
@@ -2,46 +2,16 @@
 
 namespace Multifabriken
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-             Produkter beställning = new Produkter();
-
-             Console.Clear();
-             Console.WriteLine("Välkommen till Multifabriken, ta en titt på vår meny:\n");
-             System.Console.WriteLine("[1] Beställa produkter");
-             System.Console.WriteLine("[2] Se beställda produkter");
-             System.Console.WriteLine("[3] Avsluta");
-             int input = int.Parse(Console.ReadLine());
-
-             switch(input)
+        
+             static void Main(string[] args)
              {
-                 case 1: 
-                // Beställa produkter
-                 Console.Clear();
-                 beställning.Order();
-                 break;
-
-                 case 2:
-                 //Historik
-                 Console.Clear();
-                  
-    
-                 break;
-
-                 case 3: 
-                 return;
-
-                 default:
-
-                 System.Console.WriteLine("Felaktig inmatning.");
-
-                 break;
-
+                 Produkter produkter = new Produkter();
+                 produkter.Huvudmeny(); 
              }
-    
+            
             
          }
-    }
+    
 }
